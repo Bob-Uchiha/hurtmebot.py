830d7816f9e5f216e648dffdcbd849d8aecfd14f
Bob Uchiha <dalekofawesome@gmail.com>
2022-07-13 13:40:44 -0700

    init

---
+++ cogs/Manager.py
@@ -0,0 +1,31 @@
+import discord from discord.ext import commands
+
+class Manager(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+
+    @commands.command()
+    async def bred(self,ctx):
+        await ctx.send('cheese')
+    @commands.command()
+    async def kick(self,ctx,member: discord.Member, *, reason=None):
+        await member.kick(reason=reason)
+    @commands.command()
+    async def ban(self,ctx,member: discord.Member, *, reason=None):
+        await member.ban(reason=reason)
+    @commands.command()
+    async def unban(self,ctx,*,member):
+        banned_users = await ctx.guild.bans()
+        member_name,member_discriminator = member.split('#')
+
+        for ban_entry in banned_users:
+            user = ban_entry.user
+
+            if(user.name, user.discriminator) == (member_name, member_discriminator):
+                await ctx.guild.unban(user)
+                await ctx.send(f'Unbanned{user.mention}')
+def setup(client):
+    client.add_cog(Manager(client))
+
---
+++ main.py
@@ -0,0 +1,37 @@
+import discord
+from discord.ext import commands
+import os
+from dotenv import load_dotenv
+import random
+import time
+load_dotenv()
+client = commands.Bot(command_prefix='n.')
+TOKEN = os.getenv('TOKEN')
+@client.command
+async def load(ctx, extension):
+    client.load_extension(f'cogs.{extension}')
+@client.command
+async def unload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        client.load_extension(f'cogs.{filename[:-3]}')
+@client.event
+async def on_ready():
+    print('logged in as {0.user}'.format(client))
+@client.command()
+async def ping(ctx):
+    await ctx.send(f'Your current latency is {round(client.latency * 1000)}ms')
+
+@client.command(aliases=['die','sixsideddie','_6die'])
+async def _6d(ctx):
+    response = (f'{random.randrange(1,6)}')
+    await ctx.send(response)
+@client.command()
+async def clear(ctx, amount=10):
+    await ctx.channel.purge(limit=amount+1)
+
+
+
+
+client.run(TOKEN)
